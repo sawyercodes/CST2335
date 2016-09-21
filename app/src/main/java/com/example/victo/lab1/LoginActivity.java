971d6de866191dd04e6fcacec5e33b78fa012002
Victoria Sawyer <victoria.sawyer@hotmail.com>
2016-09-21 17:48:19 -0400

    Updated to correct Lab 2 content

--- app/src/main/java/com/example/victo/lab1/LoginActivity.java
+++ app/src/main/java/com/example/victo/lab1/LoginActivity.java
@@ -31,47 +31,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        textViewEmail = (TextView)findViewById(R.id.textViewEmail);
-        SharedPreferences preferences = getSharedPreferences(SETTINGS, MODE_PRIVATE);
-        emailStored = preferences.getString(EMAILSTOREDSETTING, emailStored);
-        textViewEmail.setText(R.string.email_stored);
-
-
-        Log.i(ACTIVITY_NAME,"In onCreate()");
     }
-
-    protected void onResume() {
-        super.onResume();
-        Log.i(ACTIVITY_NAME,"In onResume()");
-
-    }
-
-    protected void onStart() {
-        super.onStart();
-        Log.i(ACTIVITY_NAME,"In onStart()");
-    }
-
-    protected void onPause() {
-        super.onPause();
-        Log.i(ACTIVITY_NAME,"In onPause()");
-    }
-
-    protected void onStop() {
-        super.onStop();
-        Log.i(ACTIVITY_NAME,"In onStop()");
-    }
-
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(ACTIVITY_NAME,"In onDestroy()");
-    }
-
-    protected static final String ACTIVITY_NAME = "LoginActivity";
-    private static final String SETTINGS = "com.example.victo.lab1.user_input";
-    private static final String EMAILSTOREDSETTING = "com.example.victo.lab1.email_stored";
-    private Button buttonLogin = (Button)findViewById(R.id.buttonLogin);
-    private String emailStored = "Stored Email from LoginActivity.java";
-    private TextView textViewEmail;
-
 
 }
--- app/src/main/java/com/example/victo/lab1/StartActivity.java
+++ app/src/main/java/com/example/victo/lab1/StartActivity.java
@@ -10,34 +10,5 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Log.i(ACTIVITY_NAME,"In onCreate()");
     }
-
-    protected void onResume() {
-        super.onResume();
-        Log.i(ACTIVITY_NAME,"In onResume()");
-
-    }
-
-    protected void onStart() {
-        super.onStart();
-        Log.i(ACTIVITY_NAME,"In onStart()");
-    }
-
-    protected void onPause() {
-        super.onPause();
-        Log.i(ACTIVITY_NAME,"In onPause()");
-    }
-
-    protected void onStop() {
-        super.onStop();
-        Log.i(ACTIVITY_NAME,"In onStop()");
-    }
-
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(ACTIVITY_NAME,"In onDestroy()");
-    }
-
-    protected static final String ACTIVITY_NAME = "StartActivity";
 }
