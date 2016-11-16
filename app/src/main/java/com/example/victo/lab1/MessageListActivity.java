42d6233ce3470fd1397aaa075c8f1054542f5f29
Victoria Sawyer <victoria.sawyer@hotmail.com>
2016-11-15 23:51:21 -0500

    Finished Lab 7

---
+++ app/src/main/java/com/example/victo/lab1/MessageListActivity.java
@@ -0,0 +1,240 @@
+package com.example.victo.lab1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import com.example.victo.lab1.dummy.DummyContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An activity representing a list of Messages. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link MessageDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class MessageListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        Resources resources = getResources();
+        Context context = getApplicationContext();
+
+        final ListView listViewChat = (ListView) findViewById(R.id.listViewChat);
+        final ChatAdapter chatAdapter = new ChatAdapter(this);
+        listViewChat.setAdapter(chatAdapter);
+        final EditText editTextChat = (EditText) findViewById(R.id.editTextChat);
+        Button buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        ChatDatabaseHelper chatDBHelper = new ChatDatabaseHelper(context);
+        chatDB = chatDBHelper.getWritableDatabase();
+        final ContentValues cValues = new ContentValues();
+
+        Cursor cursor = chatDB.query(ChatDatabaseHelper.TABLE_NAME, new String[]{ChatDatabaseHelper.KEY_ID, ChatDatabaseHelper.KEY_MESSAGE}, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                String message = cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE));
+                chatArray.add(message);
+                Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+                cursor.moveToNext();
+            } while (!cursor.isAfterLast());
+        }
+
+        Log.i(ACTIVITY_NAME, "Cursor's column count=" + cursor.getColumnCount());
+        for (int i=0; i<cursor.getColumnCount(); i++) {
+            Log.i(ACTIVITY_NAME, cursor.getColumnName(i));
+        }
+
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String chatString = editTextChat.getText().toString();
+                chatArray.add(chatString);
+                chatAdapter.notifyDataSetChanged();
+                editTextChat.setText("");
+                cValues.put("message", chatString);
+                chatDB.insert(ChatDatabaseHelper.TABLE_NAME, null, cValues);
+            }
+        });
+
+
+        if (findViewById(R.id.message_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<DummyContent.DummyItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.message_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(MessageDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        MessageDetailFragment fragment = new MessageDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.message_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, MessageDetailActivity.class);
+                        intent.putExtra(MessageDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public DummyContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+
+
+    private class ChatAdapter extends ArrayAdapter<String> {
+
+        public ChatAdapter(Context context) {
+            super(context, 0);
+        }
+
+        public int getCount() {
+            return chatArray.size();
+        }
+
+        public String getItem(int position) {
+            return chatArray.get(position);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = MessageListActivity.this.getLayoutInflater();
+            View result = null;
+            if (position%2 == 0) {
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+            } else {
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+            }
+
+            TextView message = (TextView) result.findViewById((R.id.messageText));
+
+            final String messageText = getItem(position);
+            message.setText(messageText);
+
+            result.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(MessageDetailFragment.ARG_ITEM_ID, messageText);
+                        MessageDetailFragment fragment = new MessageDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.message_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, MessageDetailActivity.class);
+                        intent.putExtra(MessageDetailFragment.ARG_ITEM_ID, messageText);
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+
+            return result;
+        }
+
+    }
+    protected static final String ACTIVITY_NAME = "MessageListActivity";
+    private static SQLiteDatabase chatDB;
+    final ArrayList<String> chatArray = new ArrayList<>();
+}
